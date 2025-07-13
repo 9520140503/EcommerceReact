89626f4be1168b54e1229ae85f5def8468bb966c
Vishesh Raj <visheshrajput47@gmail.com>
2025-07-13 10:30:46 +0530

    complete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Logout from "./Logout"
+import { FaBars } from 'react-icons/fa'
+
 
 function Header() {
     const authStatus = useSelector((state) => state.auth.status)
@@ -15,14 +17,19 @@ function Header() {
     {name: 'Sign Up', slug: '/signup',status:!authStatus},
   ]
 
+  const [showHam,setShowHam] = useState(false)
+
   
   return (
     <div>
-        <nav className='flex items-center justify-between w-full py-5 px-2 sm:px-6 md:px-20 bg-blue-950 fixed z-50 left-0 shadow-lg shadow-purple-300'>
+        <nav className='w-full rounded-2xl border-4 border-purple-300 flex items-center justify-between py-5 px-2 sm:px-6 md:px-20 bg-blue-950 fixed z-50 left-0 shadow-lg shadow-purple-300 gap-x-5'>
           <Link to='/'>
           <h2 className='text-xl sm:text-3xl md:4xl text-white font-medium '>Loome</h2>
           </Link>
-          <ul className=' flex items-center sm:text-lg md:text-xl gap-2 sm:gap-4 md:gap-16 text-blue-300'>
+
+          <button onClick={() =>setShowHam(prev => !prev)} className='text-white sm:hidden absolute right-2 top-6'><FaBars/></button>
+
+          <ul className='hidden  sm:flex items-center sm:text-lg md:text-xl gap-2 sm:gap-4 md:gap-16 text-blue-300'>
             {navItems.map((item) => (
               item.status ?
               <li 
@@ -38,8 +45,27 @@ function Header() {
               <Logout/>
             </li>
             }
-           
           </ul>
+
+            {showHam && 
+             <ul className='sm:hidden items-center sm:text-lg md:text-xl gap-2 sm:gap-4 md:gap-16 text-blue-300'>
+            {navItems.map((item) => (
+              item.status ?
+              <li 
+              key={item.slug}
+              className='hover:bg-purple-800 transition duration-500 p-1 rounded'>
+                <Link to={item.slug}>
+                  {item.name}
+                </Link>
+              </li> : null
+            ))}
+            {authStatus && 
+            <li className='hover:bg-purple-800 transition duration-500 p-1 rounded'>
+              <Logout/>
+            </li>
+            }
+          </ul>}
+
         </nav>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        snake:{
+           '0%': { backgroundPosition: '0% 50%' },
+           '100%': { backgroundPosition: '200% 50%' },
+        }
+      },
+      animation:{
+        snake:'snake 3s linear infinite'
+      }
+    },
   },
   plugins: [],
 }
